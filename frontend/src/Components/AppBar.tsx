cf405dd136aae7837788f98670bc55e32ccd8625
Taylor <taylorknoah@gmail.com>
2022-05-10 16:08:55 -0700

    updated appbar appearance

--- frontend/src/Components/AppBar.tsx
+++ frontend/src/Components/AppBar.tsx
@@ -1,34 +1,59 @@
-import {Box, AppBar as Bar, Toolbar, IconButton, Typography} from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu';
+import {Box, AppBar as Bar, Toolbar, Typography, Button, ButtonGroup} from '@mui/material'
 import { APPBAR } from '../Constants';
 
 const AppBar = () => {
-	return(
+
+	const buttonSX = {
+		mr: 1, 
+		textTransform: 'none',
+		borderRadius: 28
+	}
+
+	return (
 		<Box>
 			<Bar>
 				<Toolbar>
-					<Box width={0.1}>
-						<IconButton
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="menu"
-						sx={{ mr: 2 }}
-						>
-							<MenuIcon />
-						</IconButton>
-					</Box>
-
-					<Box display='flex' width={1} justifyContent='center'>
+					<Box display='flex' width={1} justifyContent='left'>
 						<Typography variant='h4'>
 							{APPBAR.TITLE}
 						</Typography>
 					</Box>
+
+					<Box width={1} display='flex' justifyContent='center'>
+						<ButtonGroup size='medium'  color='success' >
+							<Button sx={buttonSX} variant='contained'>
+								<Typography variant="h5">
+									{APPBAR.LEND}
+								</Typography>
+							</Button>
+							<Button sx={buttonSX} variant='contained'>
+								<Typography variant="h5">
+									{APPBAR.REQUEST}
+								</Typography>
+							</Button>
+						</ButtonGroup>
+						
+					</Box>
+
+					<Box width={1} display='flex' justifyContent='right'>
+						<Button sx={buttonSX} size='medium' variant='contained' color='info'>
+							<Typography variant="subtitle1" color='white'>
+								{APPBAR.PROFILE}
+							</Typography>
+						</Button>
+						<Button sx={buttonSX} size='medium' variant='contained' color='info'>
+							<Typography variant="subtitle1" color='white'>
+								{APPBAR.LOGIN}
+							</Typography>
+						</Button>
+					</Box>
+
 				</Toolbar>
 			</Bar>
 		</Box>
 	
 	);
 }
+
 
 export default AppBar;
--- frontend/src/Constants.ts
+++ frontend/src/Constants.ts
@@ -1,3 +1,7 @@
 export const APPBAR = {
-	TITLE:  'Lend A Hand !'
+	TITLE:  'Lend a hand',
+	LEND: 'Lend',
+	REQUEST: 'Request',
+	PROFILE: 'Profile',
+	LOGIN: 'Login'
 }
