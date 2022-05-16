2583947bf680a439ea89e429f9199399331ff7ef
kesquerra <kyle.esquerra.dev@gmail.com>
2022-05-16 16:21:47 -0700

    add item model

--- backend/internal_api/src/models/item.rs
+++ backend/internal_api/src/models/item.rs
@@ -1,10 +1,14 @@
-use chrono::DateTime;
+use chrono::{DateTime, Local};
+use sqlx::FromRow;
+use serde::{Serialize, Deserialize};
 
+
+#[derive(Serialize, Deserialize, FromRow)]
 pub struct Item {
     id: i32,
     name: String,
     is_lent_item: bool,
     img_uri: String,
-    lend_start: DateTime,
-    lend_end: DateTime
+    lend_start: DateTime<Local>,
+    lend_end: DateTime<Local>
 }
