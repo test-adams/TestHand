8c03cf438daa16f1a5886da29100a896ff43165d
Taylor <taylorknoah@gmail.com>
2022-05-19 17:02:34 -0700

    init lend page list. using mock data.

--- frontend/src/Components/LendPage.tsx
+++ frontend/src/Components/LendPage.tsx
@@ -1,7 +1,48 @@
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Paper, Button } from "@mui/material";
 import { LEND } from "../Constants";
+import { LendMock } from "../MockData";
+
+
+interface cardProps {
+	item: string,
+	lend_time: string,
+	lender: string,
+	imguri?: string
+}
+
+
+const ItemCard = (props: cardProps) => {
+	return(
+		<Box sx={{ p:1, border: '1px solid', borderRadius: '10px'	}} display='flex'>
+			<Box>
+				<img alt = 'card' width={200} height={200} src={props.imguri}/>
+			</Box>
+			<Box ml={2} mr={2}>
+				<Typography variant='h5'>Item: {props.item}</Typography>
+				<Typography variant='h5'>Lend Length: {props.lend_time}</Typography>
+				<Typography variant='h5'>Lender: {props.lender}</Typography>
+				<br/><br/>
+				<Button size='large' variant='contained'>Request!</Button>
+			</Box>
+		</Box>
+	);
+}
 
 const LendPage = () => {
+	const mockProps1: cardProps = {
+		item: LendMock.Card1.name,
+		lend_time: LendMock.Card1.end_time,
+		lender: LendMock.Card1.lender,
+		imguri: LendMock.Card1.imguri
+	}
+
+	const mockProps2: cardProps = {
+		item: LendMock.Card2.name,
+		lend_time: LendMock.Card2.end_time,
+		lender: LendMock.Card2.lender,
+		imguri: LendMock.Card2.imguri
+	}
+
 	return(
 		<>
 			<Box mt={15} display='flex' justifyContent='center'>
@@ -14,6 +55,21 @@ const LendPage = () => {
 				<Typography variant='h5'>
 					{LEND.subtitle}
 				</Typography>
+			</Box>
+
+			<Box mt={10} display='flex' justifyContent='center'>
+				<Paper elevation={10}>
+					<Box sx={{p:2}} display='flex' justifyContent='center'>
+						<Typography sx={{textDecoration: 'underline'}} variant='h4' color='primary'>
+							Items ready to be lent!
+						</Typography>
+					</Box>
+					<Box sx={{p:2}}>
+						<ItemCard {...mockProps1}/>
+						<br/>
+						<ItemCard {...mockProps2}/>
+					</Box>
+				</Paper>
 			</Box>
 		</>
 	);
---
+++ frontend/src/MockData.ts
@@ -0,0 +1,21 @@
+export const LendMock = {
+	Card1: {
+		id: '123',
+		name: 'Ladder',
+		lend_item: true,
+		imguri: 'https://ii.worldmarket.com/fcgi-bin/iipsrv.fcgi?FIF=/images/worldmarket/source/52760_XXX_v1.tif&wid=480&cvt=jpeg',
+		start_time: 'start',
+		end_time: '3 Days',
+		lender: 'user123'
+	},
+
+	Card2: {
+		id: '1337',
+		name: 'Getting rid of extra eggs.',
+		lend_item: true,
+		imguri: 'https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg',
+		start_time: 'start',
+		end_time: 'none',
+		lender: 'totally-reliable-guy'
+	}
+}
